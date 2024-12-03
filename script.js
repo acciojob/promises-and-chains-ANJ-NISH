cc6fed45e7c80ec4867432618b85ebaa17257842
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-12-03 12:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let votepromise=(name,age)=>new Promise((resolve, reject)=>{
+	if(age>18)
+	{
+		setTimeout(()=>resolve(`Welcome, ${name}. You can vote`),4000);
+	}
+	else
+	{
+		reject(`Oh sorry ${name}. You aren't old enough.`);
+	}
+});
+
+document.getElementById('f1').addEventListener('submit',(e)=>
+	{
+		e.preventDefault();
+		let age=document.getElementById('age').value;
+		let name=document.getElementById('name').value;
+
+		votepromise(name,age).then((msg1)=> alert(msg1)).catch((msg2)=>alert(msg2));
+	})
